16725f8c952be0382259a2f4df247bbb6557871d
Lamithy <anderson001749@gmail.com>
2020-10-10 21:19:12 -0300

    database heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'brainstorming',
+  host: 'ec2-18-235-20-228.compute-1.amazonaws.com',
+  username: 'kymybolnkpsqwd',
+  password: 'ebf6b45ac12837c50f9db3b1b03d4278041dbd4f46a6a9493877919e91197391',
+  database: 'd2uqlmc0dm2evr',
   define: {
     timestamps: true,
     underscored: true,
